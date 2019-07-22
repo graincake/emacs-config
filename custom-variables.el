2f78db512f08fba8a00b6ea288386614c8eb95fb
onepiece <onepiece@MacBook-Air.local>
2019-07-22 22:08:56 +0800

    my config

---
+++ custom-variables.el
@@ -0,0 +1,20 @@
+
+;; function-args
+;; (require 'function-args)
+;; (fa-config-default)
+;; (define-key c-mode-map  [(tab)] 'company-complete)
+;; (define-key c++-mode-map  [(tab)] 'company-complete)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (solarized-theme zygospore yasnippet ws-butler volatile-highlights use-package undo-tree iedit helm-swoop helm-projectile helm-gtags dtrt-indent company comment-dwim-2 clean-aindent-mode anzu))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- init.el
+++ init.el
@@ -1,8 +1,24 @@
+
 (require 'package)
+(setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")))
 (add-to-list 'package-archives
          '("melpa" . "http://melpa.org/packages/") t)
 
 (package-initialize)
+
+;;
+;; save autosave and backup directory
+;;
+(defconst emacs-tmp-dir (format "%s/%s%s/" temporary-file-directory "emacs" (user-uid)))
+(setq backup-directory-alist `((".*" . ,emacs-tmp-dir)))
+(setq auto-save-file-name-transforms `((".*" ,emacs-tmp-dir t)))
+(setq auto-save-list-file-prefix emacs-tmp-dir)
+
+;;
+;; custom variable path
+;;
+(setq custom-file "~/.emacs.d/custom-variables.el")
+(load custom-file)
 
 (when (not package-archive-contents)
     (package-refresh-contents))
@@ -13,9 +29,18 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+(use-package solarized-theme
+  :config
+  (load-theme 'solarized-dark t))
+
+;;将模块化后的几个文件加载到load-path中
+(add-to-list 'load-path "~/.emacs.d/lisp/")
 (add-to-list 'load-path "~/.emacs.d/custom")
 
+;;(use-package 'solarized-theme)
+
 (require 'setup-general)
+
 (if (version< emacs-version "24.4")
     (require 'setup-ivy-counsel)
   (require 'setup-helm)
@@ -24,24 +49,44 @@
 (require 'setup-cedet)
 (require 'setup-editing)
 
+(require 'init-keybindings)
 
 
-;; function-args
-;; (require 'function-args)
-;; (fa-config-default)
-;; (define-key c-mode-map  [(tab)] 'company-complete)
-;; (define-key c++-mode-map  [(tab)] 'company-complete)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (zygospore helm-gtags helm yasnippet ws-butler volatile-highlights use-package undo-tree iedit dtrt-indent counsel-projectile company clean-aindent-mode anzu))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(show-paren-mode t)
+;;
+;; Hide Block        C-c @ C-h
+;; Show Block        C-c @ C-s
+;; Hide All          C-c @ C-M-h
+;; Show All          C-c @ C-M-s
+;; Hide Level        C-c @ C-l
+;; Toggle Hiding     C-c @ C-c
+;;
+(add-hook 'prog-mode-hook #'hs-minor-mode)
+
+;;高亮当前行
+(global-hl-line-mode 1)
+
+
+;; 显示行号
+(global-linum-mode 1)
+
+;; 关闭文件滑动控件
+(scroll-bar-mode -1)
+
+;;菜单隐藏
+(menu-bar-mode -1)
+
+;; 关闭启动帮助画面
+(setq inhibit-splash-screen 1)
+
+;; 快速打开配置文件
+(defun open-init-file()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+
+;; 这一行代码，将函数 open-init-file 绑定到 <f2> 键上
+(global-set-key (kbd "<f2>") 'open-init-file)
+
+
+;;关闭声音
+(setq ring-bell-function 'ignore)
